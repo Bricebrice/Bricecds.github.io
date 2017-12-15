0f4ef51fbbfc6a198e0cd83be8ffbfa59aa28fbb
Brice <cabioch.brice@gmail.com>
2017-12-15 12:30:31 +0100

    i think im done

--- dist/js/index.js
+++ dist/js/index.js
@@ -40,66 +40,11 @@ $('.menu--item--contacto').on('click', function(event){
       conmutarMenu(true);
 });
 
-/* $(document).ready(function() {
-    $('#slide-1').show();
-    $('#nav-1').addClass('active-cd');
-
-    function muestraSlideSiguiente() {
-        var siguienteSlide = $('.slides li:visible').next();
-        var slideActual = $('.slides li:visible');
-        slideActual.hide();
-        if (siguienteSlide.is('li')) {
-            siguienteSlide.show();
-        } else {
-            $('.slides li').first().show();
-        }
-        var siguienteBullet = $('.active-cd').next();
-        var bulletActual = $('.active-cd');
-        bulletActual.removeClass('active-cd');
-        // El siguiente elemento sigue siendo un li?
-        if (siguienteBullet.is('li')) {
-            siguienteBullet.addClass('active-cd');
-        } else {
-            // Muestra el primer elemento de nuevo
-            $('.nav li').first().addClass('active-cd');
-        }
-    };
-
-    function muestraSlideAnterior() {
-        var anteriorSlide = $('.slides li:visible').prev();
-        var slideActual = $('.slides li:visible');
-        slideActual.hide();
-        // El siguiente elemento sigue siendo un li?
-        if (anteriorSlide.is('li')) {
-            anteriorSlide.show();
-        } else {
-            // Muestra el cuarto elemento de nuevo
-            $('.slides li').last().show();
-        }
-
-        var anteriorBullet = $('.active-cd').prev();
-        var bulletActual = $('.active-cd');
-        bulletActual.removeClass('active-cd');
-        // El siguiente elemento sigue siendo un li?
-        if (anteriorBullet.is('li')) {
-            anteriorBullet.addClass('active-cd');
-        } else {
-            // Muestra el primer elemento de nuevo
-            $('.nav li').last().addClass('active-cd');
-        }
-    };
-        $('.next').click(muestraSlideSiguiente);
-        $('.prev').click(function() {
-            muestraSlideAnterior(); //es lo mismo que lo de arriba
-        });
-
-
-        $(document).keydown(function(event) {
-            if (event.keyCode == 37) {
-                muestraSlideAnterior();
-            } else if (event.keyCode == 39) {
-                muestraSlideSiguiente();
-            }
-        });
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 1300 ) {
+        $('.scrolltop:hidden').stop(true, true).fadeIn();
+    } else {
+        $('.scrolltop').stop(true, true).fadeOut();
+    }
 });
-*/
+$(function(){$(".scroll").click(function(){$("html,body").animate({scrollTop:$(".thetop").offset().top},"1000");return false})})
